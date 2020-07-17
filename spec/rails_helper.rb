01da29188677ea2886ec527203d5df33cbfdbe66
j-jasmin <jasmin.jenull@gmx.at>
2020-07-17 10:49:56 +0200

    testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,6 @@ require File.expand_path('../config/environment', __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'database_cleaner'
 require 'capybara/rspec'
 
 begin
@@ -20,6 +19,4 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-
-
 end
