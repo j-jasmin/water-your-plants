c7f62bf09ce6de8767ccd962f7d5618a49ee4254
j-jasmin <jasmin.jenull@gmx.at>
2020-07-17 12:52:56 +0200

    testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,7 +43,9 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-  config.active_record.maintain_test_schema = false
+  config.active_record.maintain_test_schema = true
+
+  DatabaseCleaner.allow_remote_database_url = true
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
