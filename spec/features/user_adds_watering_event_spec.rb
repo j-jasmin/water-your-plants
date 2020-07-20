a80228a8e1023d02574942b40f07c28b919df2c1
j-jasmin <jasmin.jenull@gmx.at>
2020-07-20 19:26:12 +0200

    another one..

--- spec/features/user_adds_watering_event_spec.rb
+++ spec/features/user_adds_watering_event_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a watering event', type: :feature do
-  scenario 'successfully' do
+  before do
     login_as(FactoryBot.create(:user))
-    plant = FactoryBot.build(:plant)
+    @plant = FactoryBot.build(:plant)
+    @plant.update_attribute(:id, 7)
+  end
+
+  scenario 'successfully' do
     visit new_plant_path
     fill_in 'plant[common_name]', with: 'Monstera', visible: false
     click_on 'Create Plant'
     visit root_path
     page.status_code == 200
-    click_on " #{plant.common_name} "
+    visit "plants/#{@plant.id}"
     fill_in 'watering_event_date', with: '12/12/2019'
     click_on 'Create Watering event'
     page.status_code == 302
