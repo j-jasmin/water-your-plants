6f26ce69309c8d07174764078e425c3e81ea1e25
j-jasmin <jasmin.jenull@gmx.at>
2020-08-05 14:26:19 +0200

    ...

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,2 @@
-if ENV["REDISTOGO_URL"]
-  #uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
-  #REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-  $redis = Redis.new(url: ENV["REDISTOGO_URL"])
-end
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
