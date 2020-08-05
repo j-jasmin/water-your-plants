456b1c05fc2142274b0c620403614c5294ca4e35
j-jasmin <jasmin.jenull@gmx.at>
2020-08-05 15:50:19 +0200

    ...

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,27 @@
-Sidekiq.configure_server do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'],
-    port: ENV['REDIS_PORT'] || '6379'
-  }
+if Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.redis = {
+      host: ENV['REDIS_HOST'],
+      port: ENV['REDIS_PORT'] || '6379'
+    }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      host: ENV['REDIS_HOST'],
+      port: ENV['REDIS_PORT'] || '6379'
+    }
+  end
 end
 
-Sidekiq.configure_client do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'],
-    port: ENV['REDIS_PORT'] || '6379'
-  }
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 2 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 20 }
+  end
 end
 
 schedule_file = "config/schedule.yml"
