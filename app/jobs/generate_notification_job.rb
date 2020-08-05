7c1cd20ed8950a71ce8ae1f72054281a59a57d91
j-jasmin <jasmin.jenull@gmx.at>
2020-08-05 13:51:17 +0200

    ...

--- app/jobs/generate_notification_job.rb
+++ app/jobs/generate_notification_job.rb
@@ -5,7 +5,7 @@ class GenerateNotificationJob < ApplicationJob
     plants = Plant.all
     plants.each do |plant|
       next_event = plant.watering_events.sort { |a, b| b.date <=> a.date }[0][:date] + plant.watering_interval
-      Notification.create(date: Date.today, plant: plant) if next_event == Date.today
+      Notification.create!(date: Date.today, plant: plant) if next_event == Date.today
     end
   end
 end
