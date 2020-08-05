146fbfbbc725cc585c13ebd44c804e290f57c54d
j-jasmin <jasmin.jenull@gmx.at>
2020-08-05 22:58:24 +0200

    ...

--- app/jobs/generate_notification_job.rb
+++ app/jobs/generate_notification_job.rb
@@ -4,12 +4,13 @@ class GenerateNotificationJob < ApplicationJob
   def perform
     plants = Plant.all
     plants.each do |plant|
-    #   next_event = plant.watering_events.sort { |a, b| b.date <=> a.date }[0][:date] + plant.watering_interval
-    #   if next_event == Date.today
-    #     notification = Notification.new(date: Date.today, plant: plant)
-    #     notification.save if notification.valid?
-    #   end
-      p plant.watering_events.sort { |a, b| b.date <=> a.date }[0][:date]
+      if plant.watering_events.present? && plant.watering_interval.present?
+        next_event = plant.watering_events.sort { |a, b| b.date <=> a.date }[0][:date] + plant.watering_interval
+        if next_event == Date.today
+          notification = Notification.new(date: Date.today, plant: plant)
+          notification.save if notification.valid?
+        end
+      end
     end
   end
 end
