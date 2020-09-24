4fc3ef9f48f37de3a2597b55288e71d33f10198c
j-jasmin <jasmin.jenull@gmx.at>
2020-09-24 14:30:30 +0200

    ...

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -6,10 +6,10 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-echo '--- run yarn install'
+echo '--- run yarn install ---'
 yarn install
 
-echo '--- run bundle install'
+echo '--- run bundle install ---'
 bundle check || bundle install
 
 bundle exec rails s -b 0.0.0.0
